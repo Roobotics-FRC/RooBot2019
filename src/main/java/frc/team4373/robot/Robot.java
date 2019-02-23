d09cbc8f0b438a9dc7921409f317c6c355410425
Sam <samgauck@icloud.com>
2019-02-23 17:09:19 -0500

    Add secondBot boolean variable

--- src/main/java/frc/team4373/robot/Robot.java
+++ src/main/java/frc/team4373/robot/Robot.java
@@ -33,8 +33,14 @@ public class Robot extends TimedRobot {
         while (port < 8) {
             solenoids[2][port] = new DoubleSolenoid(2, port++, port++);
         }
-        solenoids[3][0] = new DoubleSolenoid(3, 0, 2);
-        solenoids[3][2] = new DoubleSolenoid(3, 1, 7);
+        boolean secondBot = true;
+        if (!secondBot) {
+            solenoids[3][0] = new DoubleSolenoid(3, 0, 2);
+            solenoids[3][2] = new DoubleSolenoid(3, 1, 7);
+        } else {
+            solenoids[3][0] = new DoubleSolenoid(3, 0, 1);
+            solenoids[3][2] = new DoubleSolenoid(3, 2, 3);
+        }
     }
 
     /**
